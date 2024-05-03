12fef251e1f4e6bdd87ebd653f2a52bde64a6881
Antonino <drakon9927@libero.it>
2024-05-03 15:18:00 +0200

    M1

---
+++ script.js
@@ -0,0 +1,11 @@
+numeriDaIndovinare = [];
+
+while (numeriDaIndovinare.length < 5) {
+    let numero = Math.floor(Math.random() * 100);
+    if (!numeriDaIndovinare.includes(numero)) { 
+        numeriDaIndovinare.push(numero); 
+    }
+}
+
+  document.querySelector("header").innerHTML = `<div>${numeriDaIndovinare}</div>`;
+
